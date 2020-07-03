2c02e645b832ea28b46a65698ff0993e28592f78
janitha_ch <janithconline@gmail.com>
2020-07-03 09:58:50 +0530

    commit on master

---
+++ src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -0,0 +1,7 @@
+package com.example.ec.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
+    
+}
